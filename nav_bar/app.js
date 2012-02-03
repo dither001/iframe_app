2375f5ebcef5e9736ba468966882efe8b2ac4d04
James A. Rosen <james@zendesk.com>
2012-02-03 12:07:15 -0800

    adding apps from Lotus

---
+++ nav_bar/app.js
@@ -0,0 +1,50 @@
+//= require ./app.js
+
+(function() {
+
+  Zendesk.Apps.NavBarApp = Zendesk.Apps.App.extend({
+    location: Zendesk.Apps.Site.NAV_BAR,
+    appID: '/apps/01-nav_bar/versions/1.0.0',
+    eventuallyEvents: {
+      'click a': '',
+      'keyup(ESCAPE) @form': 'formCancelled'
+    },
+    defaultSheet: 'inactive',
+
+    templates: {
+      main: '<img src="{{hoverIconURL}}" title="{{label}}" data-sheet-name="hover" />' +
+            '<img src="{{inactiveIconURL}}" title="{{label}}" data-sheet-name="inactive" />' +
+            '<img src="{{selectedIconURL}}" title="{{label}}" data-sheet-name="selected" />',
+      iframe: '<header><h1>{{name}}</h1></header><iframe src="{{iframeURL}}" class="pane"/>'
+    },
+
+    events: {
+      'mouseover %inactive': '%hover',
+      'mouseout  %hover':    '%inactive',
+      paneswitch: function(event, newPane) {
+        if (newPane !== this.config.name) {
+          this.showSheet('inactive');
+        } else {
+          this.showSheet('selected');
+        }
+      },
+      click: function() {
+        this.host.goToPath('apps/%@'.fmt(this.config.name));
+        this.showSheet('selected');
+      }
+    },
+
+    launch: function(host, config) {
+
+      var context = $.extend({}, config, {
+        template: this.template('iframe'),
+        classNames: 'custom'
+      });
+
+      var paneContext = $.extend({}, context);
+
+      host.addPane(paneContext);
+    }
+  });
+
+}());
