07e5cbee581726f7336234144bf49c7b03df376a
James A. Rosen <james@zendesk.com>
2012-05-30 14:44:29 -0700

    pre-built apps: replace closures so jshint checks pass
    In the future, once https://www.pivotaltracker.com/story/show/30440883
    is done, the jshint checks will be done in the builder and we can
    turn off the checks for these files in CI.

--- nav_bar/app.js
+++ nav_bar/app.js
@@ -1,25 +1,29 @@
-{
-  appID: '/apps/01-nav_bar/versions/1.0.0',
-  defaultState: 'inactive',
+(function() {
 
-  events: {
-    'mouseover %inactive': '%hover',
-    'mouseout  %hover':    '%inactive',
-    paneswitch: function(event, newPane) {
-      if (newPane !== this.settings.name) {
-        this.switchTo('inactive');
-      } else {
+  return {
+    appID: '/apps/01-nav_bar/versions/1.0.0',
+    defaultState: 'inactive',
+
+    events: {
+      'mouseover %inactive': '%hover',
+      'mouseout  %hover':    '%inactive',
+      paneswitch: function(event, newPane) {
+        if (newPane !== this.settings.name) {
+          this.switchTo('inactive');
+        } else {
+          this.switchTo('selected');
+        }
+      },
+      click: function() {
+        this.host.goToPath(helpers.fmt('apps/%@', this.settings.name));
         this.switchTo('selected');
       }
     },
-    click: function() {
-      this.host.goToPath(helpers.fmt('apps/%@', this.settings.name));
-      this.switchTo('selected');
-    }
-  },
 
-  launch: function(host, settings) {
-    host.addPane(settings.name,
-                 this.renderTemplate('iframe'), {name: settings.name});
-  }
-}
+    launch: function(host, settings) {
+      host.addPane(settings.name,
+                   this.renderTemplate('iframe'), {name: settings.name});
+    }
+  };
+
+}());
