c99f74a9dcdf35d21ffee5ca60f3ee551ad7693c
Jake Holman <jake@zendesk.com>
2013-03-29 17:13:41 -0700

    [ZD#303298] Prevents app icons flashing or taking a long time to load in when hovering/clicking, prevents multiple http requests for the same images

--- app.js
+++ app.js
@@ -1,35 +1,58 @@
 (function() {
 
   return {
-    defaultState: 'inactive',
+    defaultState: 'icons',
 
     // Local vars
     iframeIsInDOM: false,
 
     events: {
-      'mouseover %inactive': '%hover',
-      'mouseout  %hover':    '%inactive',
+      'mouseenter %icons': 'iconHover',
+      'mouseleave %icons': 'iconInactive',
+
       paneswitch: function(event, newPane) {
-        if (newPane !== this.setting('title')) {
-          this.switchTo('inactive');
+        if (newPane !== this.appTitle()) {
+          this.iconInactive(true);
         } else {
-          this.switchTo('selected');
+          this.iconSelected();
         }
       },
+
       click: function() {
         if (!this.iframeIsInDOM) { this.addIframeToDOM(); }
-        this.host.goToPath(helpers.fmt( 'apps/%@', this.generateTitle() ));
-        this.switchTo('selected');
+        this.host.goToPath(helpers.fmt( 'apps/%@', this.appTitle() ));
+        this.iconSelected();
       }
     },
 
-    generateTitle: function() {
+    iconHover: function() {
+      if (this.appIsFocused()) { return; }
+      this.$('.inactive, .selected').hide();
+      this.$('.hover').show();
+    },
+
+    iconInactive: function(forceSwitch) {
+      if (forceSwitch !== true && this.appIsFocused()) { return; }
+      this.$('.selected, .hover').hide();
+      this.$('.inactive').show();
+    },
+
+    iconSelected: function() {
+      this.$('.inactive, .hover').hide();
+      this.$('.selected').show();
+    },
+
+    appIsFocused: function() {
+      return this.$(".selected").is(':visible');
+    },
+
+    appTitle: function() {
       return this.setting('title').toLowerCase().split(' ').join('-');
     },
 
     addIframeToDOM: function() {
-      this.host.addPane(this.generateTitle(),
-        this.renderTemplate('iframe'), { name: this.generateTitle() });
+      this.host.addPane(this.appTitle(),
+        this.renderTemplate('iframe'), { name: this.appTitle() });
 
       this.iframeIsInDOM = true;
     }
