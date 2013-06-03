d67841c0ccc5b3d441021f98ec4775e27e3f9264
Daniel Ribeiro <djbreves@gmail.com>
2013-06-03 13:27:11 +1000

    [APPS-594] Rewritten to comply with nav bar apps v1

--- app.js
+++ app.js
@@ -1,61 +1,21 @@
 (function() {
 
   return {
-    defaultState: 'icons',
 
-    // Local vars
-    iframeIsInDOM: false,
+    defaultState: 'iframe',
 
     events: {
-      'mouseenter': 'iconHover',
-      'mouseleave': 'iconInactive',
+      'app.activated':  'appActivated'
+    },
 
-      paneswitch: function(event, newPane) {
-        if (newPane !== this.appTitle()) {
-          this.iconInactive(true);
-        } else {
-          this.iconSelected();
-        }
-      },
-
-      click: function() {
-        if (!this.iframeIsInDOM) { this.addIframeToDOM(); }
-        this.host.goToPath(helpers.fmt( 'apps/%@', this.appTitle() ));
-        this.iconSelected();
+    appActivated: function(data) {
+      if (data.firstLoad && this.currentLocation() === 'nav_bar') {
+        this.setIconState('active', this.setting('selectedIconURL'));
+        this.setIconState('inactive', this.setting('inactiveIconURL'));
+        this.setIconState('hover', this.setting('hoverIconURL'));
       }
-    },
-
-    iconHover: function() {
-      if (this.appIsFocused()) { return; }
-      this.$('.inactive, .selected').hide();
-      this.$('.hover').show();
-    },
-
-    iconInactive: function(forceSwitch) {
-      if (forceSwitch !== true && this.appIsFocused()) { return; }
-      this.$('.selected, .hover').hide();
-      this.$('.inactive').show();
-    },
-
-    iconSelected: function() {
-      this.$('.inactive, .hover').hide();
-      this.$('.selected').show();
-    },
-
-    appIsFocused: function() {
-      return this.$(".selected").is(':visible');
-    },
-
-    appTitle: function() {
-      return this.setting('title').toLowerCase().split(' ').join('-');
-    },
-
-    addIframeToDOM: function() {
-      this.host.addPane(this.appTitle(),
-        this.renderTemplate('iframe'), { name: this.appTitle() });
-
-      this.iframeIsInDOM = true;
     }
+
   };
 
 }());
