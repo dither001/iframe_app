1129731350320d58162d977e33d6f18a5d4249b4
Daniel Ribeiro <djbreves@gmail.com>
2013-06-07 12:56:47 +1000

    Remove check for currentLocation

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
     },
 
     appActivated: function(data) {
-      if (data.firstLoad && this.currentLocation() === 'nav_bar') {
+      if (data.firstLoad) {
         this.setIconState('active', this.setting('selectedIconURL'));
         this.setIconState('inactive', this.setting('inactiveIconURL'));
         this.setIconState('hover', this.setting('hoverIconURL'));
